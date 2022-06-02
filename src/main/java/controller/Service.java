b4a9b158d7713064e9f21605e7c7d3772a251123
Minhyeok1995 <repeater2487@naver.com>
2022-06-02 19:34:16 +0900

    상품 페이지 필터 이벤트 추가

---
+++ src/main/java/controller/Service.java
@@ -0,0 +1,10 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Service {
+	
+	void execute(HttpServletRequest request, HttpServletResponse response);
+	
+}
---
+++ src/main/java/controller/itempage/itempageController.java
@@ -0,0 +1,45 @@
+package controller.itempage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Service;
+
+@WebServlet("/itemPages/*")
+public class itempageController extends HttpServlet {
+
+	ArrayList<String> nonClass;
+
+	public itempageController() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String serviceStr = request.getRequestURI().substring((request.getContextPath() + "/itemPages/").length());
+		//System.out.println(serviceStr);
+		try {
+			Service service = (Service) Class.forName("service.itempage."+serviceStr).newInstance();
+			service.execute(request, response);
+			
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/items/itempage.jsp");
+			dispatcher.forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
