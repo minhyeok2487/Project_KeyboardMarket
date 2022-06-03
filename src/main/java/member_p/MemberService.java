d387a5cfc8762f4c773a30a8e53a93c8c0bfb78c
gogorungo <96456860+gogorungo@users.noreply.github.com>
2022-06-03 13:40:36 +0900

    M 컨트롤러 해결

--- src/main/java/member_p/MemberService.java
+++ src/main/java/member_p/MemberService.java
@@ -5,10 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-/**
- * Servlet implementation class F_Controller
- */
-@WebServlet("/member/*")
+
 public interface MemberService {
 
 	void execute(HttpServletRequest request, HttpServletResponse response);
