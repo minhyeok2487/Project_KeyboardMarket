462a124bb02915318ef0c825aec1b91fcc6180d1
Minhyeok1995 <repeater2487@naver.com>
2022-06-04 17:29:42 +0900

    수정

---
+++ src/main/java/controller/notice/NoticeController.java
@@ -0,0 +1,57 @@
+package controller.notice;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Service;
+
+@WebServlet("/notice/*")
+public class NoticeController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	ArrayList<String> nonClass;
+
+	public NoticeController() {
+		super();
+		nonClass = new ArrayList<String>();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+
+		String serviceStr = request.getRequestURI().substring((request.getContextPath() + "/notice/").length());
+		System.out.println(serviceStr);
+
+		if (nonClass.contains(serviceStr)) {
+			request.setAttribute("noticeUrl", serviceStr);
+		} else {
+			try {
+				Service service = (Service) Class.forName("service.noticePage." + serviceStr).newInstance();
+
+				service.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/notices/noticeListpage.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+		
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+}
---
+++ src/main/java/model/notice/NoticeDAO.java
@@ -0,0 +1,83 @@
+package model.notice;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class NoticeDAO {
+
+	Connection con;
+	PreparedStatement ptmt;
+	ResultSet rs;
+	String sql;
+
+	public NoticeDAO() {
+		try {
+			Context context = new InitialContext();
+			DataSource ds = (DataSource) context.lookup("java:comp/env/qazxsw");
+			con = ds.getConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ArrayList<NoticeDTO> list() {
+		ArrayList<NoticeDTO> res = new ArrayList<NoticeDTO>();
+
+		sql = "select * from notice order by noticeNo desc";
+
+		try {
+			ptmt = con.prepareStatement(sql);
+//			ptmt.setInt(1, start);
+//			ptmt.setInt(2, limit);
+			rs = ptmt.executeQuery();
+
+			while (rs.next()) {
+				NoticeDTO dto = new NoticeDTO();
+
+				dto.setNoticeNo(rs.getInt("noticeNo"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setReg_date(rs.getTimestamp("reg_date"));
+				dto.setHits(rs.getInt("hits"));
+				System.out.println(dto);
+
+				res.add(dto);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+
+		return res;
+
+	}
+
+	public void close() {
+		if (rs != null)
+			try {
+				rs.close();
+			} catch (SQLException e) {
+			}
+		if (ptmt != null)
+			try {
+				ptmt.close();
+			} catch (SQLException e) {
+			}
+		if (con != null)
+			try {
+				con.close();
+			} catch (SQLException e) {
+			}
+
+	}
+
+}
