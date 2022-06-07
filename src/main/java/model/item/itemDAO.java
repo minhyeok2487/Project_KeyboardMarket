54f85cebb74ca46ad347e9c4834ac3c6cf8cc5f0
Minhyeok1995 <repeater2487@naver.com>
2022-06-07 21:05:06 +0900

    상품 리스트 가격범위 확인 추가

--- src/main/java/model/item/itemDAO.java
+++ src/main/java/model/item/itemDAO.java
@@ -59,6 +59,38 @@ public class itemDAO {
 		return res;
 	}
 	
+	public ArrayList<itemDTO> detaillist(int start, int end) {
+		ArrayList<itemDTO> res = new ArrayList<itemDTO>();
+		sql = "select * from item where price between ? AND ?";
+		try {
+			ptmt = con.prepareStatement(sql);
+			ptmt.setInt(1, start);
+			ptmt.setInt(2, end);
+			rs = ptmt.executeQuery();
+			while(rs.next()) {
+				itemDTO dto = new itemDTO();
+				dto.setItemNo(rs.getInt("itemNo"));
+				dto.setItem_name(rs.getString("item_name"));
+				dto.setManufacture(rs.getString("manufacture"));
+				dto.setCategory(rs.getString("category"));
+				dto.setSwitchs(rs.getString("switchs"));
+				dto.setSpec(rs.getString("spec"));
+				dto.setPrice(rs.getInt("price"));
+				dto.setStock(rs.getInt("stock"));
+				dto.setReg_date(rs.getDate("reg_date"));
+				dto.setItem_img1(rs.getString("item_img1"));
+				dto.setItem_img2(rs.getString("item_img2"));
+				dto.setItem_sold(rs.getInt("item_sold"));
+				res.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return res;
+	}
+	
 	public itemDTO Detail(int index){
 		itemDTO dto = new itemDTO();
 		sql = "select * from item where itemNo = ?";
@@ -96,4 +128,7 @@ public class itemDAO {
 		if(ptmt!=null)try {ptmt.close();} catch (Exception e) {}
 		if(con!=null)try {con.close();} catch (Exception e) {}
 	}
+
+
+	
 }
