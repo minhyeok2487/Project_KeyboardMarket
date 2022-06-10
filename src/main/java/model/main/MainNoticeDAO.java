cc1882ad5cc821fe9ec636b9cd85793f844c7ffb
Your Name <you@example.com>
2022-06-10 18:10:02 +0900

    메인페이지 주요공지 추가

---
+++ src/main/java/model/main/MainNoticeDAO.java
@@ -0,0 +1,79 @@
+package model.main;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import model.notice.NoticeDTO;
+
+public class MainNoticeDAO {
+	Connection con;
+	PreparedStatement ptmt;
+	ResultSet rs;
+	String sql;
+	
+	public MainNoticeDAO() {
+		try {
+			Context context = new InitialContext();
+			DataSource ds = (DataSource)context.lookup("java:comp/env/qazxsw");
+			con = ds.getConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<MainNoticeDTO> Currentlist() {
+		ArrayList<MainNoticeDTO> res = new ArrayList<MainNoticeDTO>();
+		sql = "select * from MainNotice";
+		try {
+			ptmt = con.prepareStatement(sql);
+			rs = ptmt.executeQuery();
+			while (rs.next()) {
+				MainNoticeDTO dto = new MainNoticeDTO();
+				dto.setMainNoticeNo(rs.getInt("MainNoticeNo"));
+				dto.setNoticeNo(rs.getInt("noticeNo"));
+				dto.setSubject(rs.getString("subject"));
+				res.add(dto);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return res;
+	}
+	
+	public boolean insert(MainNoticeDTO dto) {
+		sql = "insert into MainNotice (noticeNo ,subject) values (?,?)";
+		try {
+			ptmt = con.prepareStatement(sql);
+			ptmt.setInt(1, dto.noticeNo);
+			ptmt.setString(2, dto.subject);
+			rs = ptmt.executeQuery();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return false;
+	}
+	
+	public void close() {
+		if(rs!=null)try {rs.close();} catch (Exception e) {}
+		if(ptmt!=null)try {ptmt.close();} catch (Exception e) {}
+		if(con!=null)try {con.close();} catch (Exception e) {}
+	}
+
+	
+	
+	
+}
--- src/main/java/model/memberService/MemberLoginReg.java
+++ src/main/java/model/memberService/MemberLoginReg.java
@@ -12,8 +12,12 @@ import model.item.itemDAO;
 import model.item.itemDTO;
 import model.main.MainDAO;
 import model.main.MainDTO;
+import model.main.MainNoticeDAO;
+import model.main.MainNoticeDTO;
 import model.member.memberDAO;
 import model.member.memberDTO;
+import model.notice.NoticeDAO;
+import model.notice.NoticeDTO;
 
 public class MemberLoginReg implements Service {
 
@@ -36,7 +40,7 @@ public class MemberLoginReg implements Service {
 //		request.setAttribute("mainUrl", "alert");
 //		request.setAttribute("saveUrl", "main/pages");
 
-		//메인 페이지 수정하면서 에러떠서 임시조치
+		// 메인 페이지 수정하면서 에러떠서 임시조치
 		// 1.추천상품 리스트
 		ArrayList<MainDTO> GoodList = new MainDAO().Currentlist("추천상품");
 		ArrayList<itemDTO> GList = new ArrayList<itemDTO>();
@@ -49,9 +53,16 @@ public class MemberLoginReg implements Service {
 		for (MainDTO res : NewList) {
 			NList.add(new itemDAO().Detail(res.getItemNo()));
 		}
-		System.out.println(GList);
+
+		// 3.주요공지사항 리스트
+		ArrayList<MainNoticeDTO> NoticeList = new MainNoticeDAO().Currentlist();
+		ArrayList<NoticeDTO> M_NoticeList = new ArrayList<NoticeDTO>();
+		for (MainNoticeDTO res : NoticeList) {
+			M_NoticeList.add(new NoticeDAO().detail(res.getNoticeNo()));
+		}
 		request.setAttribute("GList", GList);
 		request.setAttribute("NList", NList);
+		request.setAttribute("M_NoticeList", M_NoticeList);
 		request.setAttribute("mainUrl", "./main/pages");
 	}
 }
