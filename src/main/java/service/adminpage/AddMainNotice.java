03141ca06eb2cc9e05977e84742b9de8824d7f17
jiho1124 <repeater2487@naver.com>
2022-06-13 19:32:32 +0900

    공지사항올리기 수정

--- src/main/java/service/adminpage/AddMainNotice.java
+++ src/main/java/service/adminpage/AddMainNotice.java
@@ -16,26 +16,27 @@ public class AddMainNotice implements Service {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
 
-		// 주요공지사항 리스트
-		ArrayList<NoticeDTO> NoticeList = new NoticeDAO().Currentlist("게시");
-		ArrayList<NoticeDTO> Main_NoticeList = new ArrayList<NoticeDTO>();
-		for (NoticeDTO res : NoticeList) {
-			Main_NoticeList.add(new NoticeDAO().detail(res.getNoticeNo()));
+
+		
+		ArrayList<MainNoticeDTO> NoticeList = new MainNoticeDAO().Currentlist();
+		ArrayList<NoticeDTO> M_NoticeList = new ArrayList<NoticeDTO>();
+		for (MainNoticeDTO res : NoticeList) {
+			M_NoticeList.add(new NoticeDAO().detail(res.getNoticeNo()));
 		}
 
-//		ArrayList<NoticeDTO> AllNoticeList = new NoticeDAO().AllList();
-//		for (int i = 0; i < AllNoticeList.size(); i++) {
-//			for (int j = 0; j < M_NoticeList.size(); j++) {
-//				if (AllNoticeList.get(i).getNoticeNo() == M_NoticeList.get(j).getNoticeNo()) {
-//					AllNoticeList.remove(i);
-//					i--;
-//				}
-//			}
-//		}
+		ArrayList<NoticeDTO> AllNoticeList = new NoticeDAO().Currentlist("게시");
+		for (int i = 0; i < AllNoticeList.size(); i++) {
+			for (int j = 0; j < M_NoticeList.size(); j++) {
+				if (AllNoticeList.get(i).getNoticeNo() == M_NoticeList.get(j).getNoticeNo()) {
+					AllNoticeList.remove(i);
+					i--;
+				}
+			}
+		}
 		
 		ArrayList<MainNoticeDTO> mainPostList = new MainNoticeDAO().CurrentPostList("게시중");
 
-		request.setAttribute("AllNoticeList", Main_NoticeList);
+		request.setAttribute("AllNoticeList", AllNoticeList);
 		request.setAttribute("mainPostList", mainPostList);
 		request.setAttribute("mainUrl", "./admins/addMainnoticepage");
 
